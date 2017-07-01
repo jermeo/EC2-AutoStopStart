32458679592a758334a176c15d0197b2a849a0bd
jpiquet <jerome.piquet@atinternet.com>
2017-07-01 14:39:10 +0200

    lambda test

--- app/index.js
+++ app/index.js
@@ -17,128 +17,132 @@ const margin = 30; // margin time in minutes, depend on the lambda schedule
 const now = new Date();
 const keyName = "Name";
 
-utils.log("------- Start -------");
+exports.handler = (event, context, callback) => {
+
+  utils.log("------- Start -------");
 
 //for all regions
-awsUtils.getRegions()
+  awsUtils.getRegions()
 
-    .then((regions) => {
+      .then((regions) => {
 
-      let tabPromise = [];
+        let tabPromise = [];
 
-      // generate promise array for each regions
-      for (const region of regions) {
-        tabPromise.push(awsUtils.getRegionsInstances({}, region));
-      }
+        // generate promise array for each regions
+        for (const region of regions) {
+          tabPromise.push(awsUtils.getRegionsInstances({}, region));
+        }
 
-      return Promise.all(tabPromise);
+        return Promise.all(tabPromise);
 
-    })
-    .then((regionInstances) => {
+      })
+      .then((regionInstances) => {
 
-      let tabPromiseStartStop = [];
+        let tabPromiseStartStop = [];
 
-      // loop over regions
-      for (const instances of regionInstances) {
+        // loop over regions
+        for (const instances of regionInstances) {
 
-        let startList = [];
-        let stopList = [];
-        let region;
+          let startList = [];
+          let stopList = [];
+          let region;
 
-        // loop over instances
-        for (const instance of instances) {
+          // loop over instances
+          for (const instance of instances) {
 
-          // check cron tag, status and time to action
-          const startCron = awsUtils.getTagValue(instance, autoStartTag);
-          const stopCron = awsUtils.getTagValue(instance, autoStopTag);
+            // check cron tag, status and time to action
+            const startCron = awsUtils.getTagValue(instance, autoStartTag);
+            const stopCron = awsUtils.getTagValue(instance, autoStopTag);
 
-          if (startCron || stopCron) {
+            if (startCron || stopCron) {
 
-            if (startCron && awsUtils.isStopped(instance) && utils.isTimeToAction(startCron, now, margin * 60)) {
+              if (startCron && awsUtils.isStopped(instance) && utils.isTimeToAction(startCron, now, margin * 60)) {
 
-              utils.log(`START needed: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} - ${instance.InstanceId} - (${stopCron})`);
+                utils.log(`START needed: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} - ${instance.InstanceId} - (${stopCron})`);
 
-              startList.push(instance);
+                startList.push(instance);
 
-            }
-            else if (stopCron && awsUtils.isRunning(instance) && utils.isTimeToAction(stopCron, now, margin * 60)) {
+              }
+              else if (stopCron && awsUtils.isRunning(instance) && utils.isTimeToAction(stopCron, now, margin * 60)) {
 
-              utils.log(`STOP needed: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} - ${instance.InstanceId} - (${stopCron})`);
+                utils.log(`STOP needed: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} - ${instance.InstanceId} - (${stopCron})`);
 
-              stopList.push(instance);
+                stopList.push(instance);
+
+              }
+              else {
+
+                utils.log(`Nothing to do with: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} (tag found but no action needed)`);
+
+              }
 
             }
             else {
 
-              utils.log(`Nothing to do with: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} (tag found but no action needed)`);
+              utils.log(`Nothing to do with: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} (tag not found)`);
 
             }
 
           }
-          else {
 
-            utils.log(`Nothing to do with: ${awsUtils.getRegionFromDNS(instance)}\\${awsUtils.getTagValue(instance, keyName)} (tag not found)`);
+          // add action for this region
+          if (!dryMode) {
 
+            // action start
+            if (startList.length > 0) {
+              region = awsUtils.getRegionFromDNS(startList[0]);
+              tabPromiseStartStop.push(awsUtils.startInstances(region, startList.map((instance) => {
+                return instance.InstanceId;
+              })));
+            }
+
+            // action stop
+            if (stopList.length > 0) {
+              region = awsUtils.getRegionFromDNS(stopList[0]);
+              tabPromiseStartStop.push(awsUtils.stopInstances(region, stopList.map((instance) => {
+                return instance.InstanceId;
+              })));
+            }
           }
 
+          // next region
         }
 
-        // add action for this region
-        if (!dryMode) {
+        // execute array of promises actions
+        return Promise.all(tabPromiseStartStop);
 
-          // action start
-          if (startList.length > 0) {
-            region = awsUtils.getRegionFromDNS(startList[0]);
-            tabPromiseStartStop.push(awsUtils.startInstances(region, startList.map((instance) => {
-              return instance.InstanceId;
-            })));
+      })
+
+      .then((tabResults) => {
+
+        // display AWS results
+        for (const result of tabResults) {
+          if (result.StoppingInstances) {
+            utils.log("Stopping InstanceId(s):");
+            for (const instance of result.StoppingInstances) {
+              utils.log(instance.InstanceId);
+            }
           }
-
-          // action stop
-          if (stopList.length > 0) {
-            region = awsUtils.getRegionFromDNS(stopList[0]);
-            tabPromiseStartStop.push(awsUtils.stopInstances(region, stopList.map((instance) => {
-              return instance.InstanceId;
-            })));
-          }
-        }
-
-        // next region
-      }
-
-      // execute array of promises actions
-      return Promise.all(tabPromiseStartStop);
-
-    })
-
-    .then((tabResults) => {
-
-      // display AWS results
-      for (const result of tabResults) {
-        if (result.StoppingInstances) {
-          utils.log("Stopping InstanceId(s):");
-          for (const instance of result.StoppingInstances) {
-            utils.log(instance.InstanceId);
+          if (result.StartingInstances) {
+            utils.log("Starting InstanceId(s):");
+            for (const instance of result.StartingInstances) {
+              utils.log(instance.InstanceId);
+            }
           }
         }
-        if (result.StartingInstances) {
-          utils.log("Starting InstanceId(s):");
-          for (const instance of result.StartingInstances) {
-            utils.log(instance.InstanceId);
-          }
-        }
-      }
 
-    })
+      })
 
-    .catch((err) => {
-      utils.log("-------------- ERROR -----------------");
-      utils.log(err);
-      utils.log("-------------- ERROR -----------------");
-    })
+      .catch((err) => {
+        utils.log("-------------- ERROR -----------------");
+        utils.log(err);
+        utils.log("-------------- ERROR -----------------");
+      })
 
-    .then(() => {
-      utils.log("------- End -------");
-    });
+      .then(() => {
+        utils.log("------- End -------");
+      });
+
+};
 
 
---
+++ app/test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const lambda = require("./index");
+
+lambda.handler(null, null, () => {
+  console.log("end");
+});
