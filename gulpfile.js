5f186073425b0e28b472593a8d1afbcc94df591d
jpiquet <jerome.piquet@atinternet.com>
2017-07-02 18:30:23 +0200

    zip with powershell & .net

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const del = require('del');
 const install = require('gulp-install');
 const runSequence = require('run-sequence');
 const awsLambda = require("node-aws-lambda");
+const run = require('gulp-run');
 
 gulp.task('clean', function() {
   return del(['./dist', './dist.zip']);
@@ -20,10 +21,21 @@ gulp.task('node-mods', function() {
       .pipe(install({production: true}));
 });
 
+// on windows, gulp zip produce a "non conform" zip -> lambda fail during the load of cron-parser module
+// and grunt-aws-lambda produce a big output (20 Mo)
+// gulp.task('zip', function() {
+//   return gulp.src('dist/**/*')
+//       .pipe(zip('dist.zip'))
+//       .pipe(gulp.dest('./'));
+// });
+
+// for windows user only, use powershell instead
+// powershell Compress-Archive dist/* dist.zip -Force not work
+// so I use .Net Framework
+const cmd='powershell Add-Type -assembly "system.io.compression.filesystem";[System.IO.Compression.Zipfile]::CreateFromDirectory("dist", "dist.zip")';
 gulp.task('zip', function() {
-  return gulp.src('dist/**/*')
-      .pipe(zip('dist.zip'))
-      .pipe(gulp.dest('./'));
+  return run(cmd).exec()
+      .pipe(gulp.dest('output'));
 });
 
 gulp.task('upload', function(callback) {
